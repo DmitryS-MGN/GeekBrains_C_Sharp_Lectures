f3dd43f071cc015e8b31c4dfe06b160c2a13a6ad
dmitry <dmitrysalamatovmgn@yandex.ru>
2023-10-08 23:00:36 +0500

    Example008 Example009 Example010 Example011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 10, 12, 33, 41, 35, 64, 57, 18, 29, 64 };
+int n = array.Length;
+int find = 64;
+int index = 0;
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
