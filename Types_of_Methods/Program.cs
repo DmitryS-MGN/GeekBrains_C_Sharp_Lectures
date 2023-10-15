cd43853ac11d1ebf5cf95422bb3b496a9381c3a2
dmitry <dmitrysalamatovmgn@yandex.ru>
2023-10-16 01:59:11 +0500

    добавил задания от лекции функции продолжение

---
+++ Types_of_Methods/Program.cs
@@ -0,0 +1,68 @@
+﻿// 1) Группа методов, которые ничего не возвращают и ничего не принимают.
+void Method1()
+{
+    System.Console.WriteLine("Автор....."); //То что происходит внутри метода.
+}
+Method1(); // Вызов метода, обязательно использование скобочек.
+
+// 2) Группа методов,  которые ничего не возвращают, но принимают какие-то аргументы.
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+Method2("Текст сообщения ");
+
+void Method21(string msg, int count) //int count - именнованный аргумент(дополнительный) счетчик.
+{
+    int i = 0;
+    while(i < count) 
+    {
+        System.Console.WriteLine(msg);
+        i++; // Увеличение счетчика на еденицу, называют инкриментом, а уменьшение счетика на еденицу называют тэкриментом. 
+                    //  !!!Если программу зацыклило, выбраться из зацикливания можно, при помощи сочетания клавиш Ctrl + C. 
+    }
+}
+Method21("Текст сообщения ", 10);
+Method21(count 5, "Текст сообщения ") // Соблюдение порядка ввода оргументов не обязательно. 
+
+// 3) Группа методов, которые  что-то возвращают, но ничего не принимают.
+int Method3() // Если метод, что-то возвращает, мы обязательно должны указать тип данных, например "int".
+{
+    return DateTime.Now.Year; //Метод обязательно должен возвращать какое-то значение.
+}
+
+int year = Method3(); // В int year будет положен результат работы метода
+{
+    System.Console.WriteLine(year);
+}
+
+
+// 4) Группа методов, которые что-то принимают и что-то возвращают.
+string Method4(int count, string text) // char - символ
+{
+    int i = 0;
+    string result = String.Empty; //Переменная куда кладется конечный результат. String.Empty - пустая строка.
+
+    while(i < count) // Пример цикла, работающий через while
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf  ");
+System.Console.WriteLine(res);
+
+
+string Method4(int count, string text) 
+{   
+    string result = String.Empty;
+    for(int i = 0; i < count; i++) // // Пример цикла, работающий через for.
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "asdf  ");
+System.Console.WriteLine(res);
---
+++ Цикл_в_цикле/Program.cs
@@ -0,0 +1,89 @@
+﻿Цикл в цикле для таблицы умножения.
+for (int i = 2; i <= 9; i++)
+{
+    for (int j = 2; j <= 9; j++)
+    {
+        System.Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    System.Console.WriteLine();
+}
+
+
+//Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы “к” заменить большими “К”, а большие “С” заменить маленькими “с”.
+
+//— Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?
+
+// string s = "qwerty" - строка и ее символы.
+//             012345  - номер символа строки.
+// s[3]           - чтобы получить конкретный символ строки, его номер указываем в квадратных скобках, после идентификатора строки.  
+
+
+string text = "Я думаю, — сказал князь, улыбаясь, — что,"
+                + "ежели бы вас послали вместо нашего милого Винценгероде"
+                + ",вы бы взяли приступом согласие прусского короля."
+                + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length; // text.Length - длина строки или количество символов в строке.
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue)
+        {
+            result = result + $"{newValue}";
+        }
+        else 
+        {
+            result = result + $"{text[i]}";
+        }
+    }
+    return result;
+}
+string newText = Replace(text,  ' ', '|');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(text,  'к', 'К');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(text,  'c', 'C');
+System.Console.WriteLine(newText);
+
+//Упорядочить массив от от меньшего к большему.
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void printArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void selectionSort(int[] array)
+
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+printArray(arr);
+selectionSort(arr);
+printArray(arr);
